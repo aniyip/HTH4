6fb30bb74e7b01030c108800bd6cb06e88fc285b
aniyip <40328793+aniyip@users.noreply.github.com>
2020-02-16 10:31:06 -0500

    Added remaining questions
    Added questions for locations 3-11

--- src/Questions.js
+++ src/Questions.js
@@ -19,6 +19,69 @@ const questions = [
         answers: ["Carroll School of Management", "Connell School of Nursing", "Lynch School of Education", "Morrissey College of Arts and Sciences"],
         correct: "Carroll School of Management",
         location: 2 // Fulton Hall
+      },
+      {
+        question:
+          "What is the name of the second floor?",
+        answers: ["Carney Hall", "Eagle’s Nest", "Hillside Dining Hall", "Stuarts Dining Hall"],
+        correct: "Eagle’s Nest",
+        location: 3 // Mcelory Commons
+      },
+      {
+        question:
+          "What play is represented on the bottom right corner of the tinted window?",
+        answers: ["Antony & Cleopatra", "Hamlet", "Julius Caesar", "King Lear"],
+        correct: "Hamlet",
+        location: 4 // Baptist Library
+      },
+      {
+        question:
+          "How many floors are there in O'neill Library?",
+        answers: ["4 floors", "4.5 floors", "5 floors", "6 floors"],
+        correct: "5 floors",
+        location: 5 // O’neill Library
+      },
+      {
+        question:
+          "Which floor is Connell school of Nursing located in Maloney Hall?",
+        answers: ["2nd floor", "3rd floor", "4th floor", "5th floor"],
+        correct: "2nd floor",
+        location: 6 // Maloney Hall
+      },
+      {
+        question:
+          "What is the name of the black box theatre located in Robsham Theatre?",
+        answers: ["Bonn Studio Theatre", "Lobo’s Theatre", "Margot Connell Theatre", "Watts Theatre"],
+        correct: "Bonn Studio Theatre",
+        location: 7 // Robsham Theatre
+      },
+      {
+        question:
+          "Where is the swimming pool located?",
+        answers: ["1st Floor", "2nd Floor", "3rd Floor", "4th Floor"],
+        correct: "1st Floor",
+        location: 8 // Margot Connell Recreation Center
+      },
+      {
+        question:
+          "Who is the statue of in front of Alumni Stadium?",
+        answers: ["Doug Flutie", "John Johnson", "Luke Kuechly", "Matt Ryan"],
+        correct: "Doug Flutie",
+        location: 9 // Alumni Stadium/Conte Forum
+      },
+      {
+        question:
+          "What is the room number of the biggest lecture hall in Merkert?",
+        answers: ["127", "130", "133", "136"],
+        correct: "127",
+        location: 10 // Merkert Chemistry Center
+      },
+      {
+        question:
+          "What is the name of the cafe bar in Stokes?",
+        answers: ["The Caramel Bar", "The Chocolate Bar", "The Hazelnut Bar", "The Vanilla Bar"],
+        correct: "The Chocolate Bar",
+        location: 11 // Stokes Hall
       }
   ];
 
